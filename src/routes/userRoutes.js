eb1e626d7222cf5f666013975fa664eac292c3de
Thien phuc <thienphuc484@gmail.com>
2025-01-20 10:12:43 +0700

    refactor: change api in user route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,9 @@
 import { login, register, getUserDetails, getUserProfilePicture, update } from '../controllers/userController.js'
 
 export default function userRoute(req, res) {
-  if (req.method === "POST" && req.url === "/api/v1/login") {
+  if (req.method === "POST" && req.url === "/api/v1/users/login") {
     login(req, res);
-  } else if (req.method === "POST" && req.url === "/api/v1/register") {
+  } else if (req.method === "POST" && req.url === "/api/v1/users/register") {
     register(req, res);
   } else if (req.method === "GET" && req.url === "/api/v1/users/details") {
     getUserDetails(req, res);
@@ -12,7 +12,7 @@ export default function userRoute(req, res) {
   } else if (req.method === "PUT" && req.url === "/api/v1/users") {
     update(req,res);
   } else {
-    res.writeHead(404);
+    res.writeHead(404, { "Content-Type" : "appliction/json"});
     res.end(JSON.stringify({ message: "Not Found" }));
   }
 }
