bca58018e7d8d40f62fd760e4dca30718456d5c7
Thien phuc <thienphuc484@gmail.com>
2025-02-05 13:20:50 +0700

    feat(router): add new router and remove old router

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import { createServer } from "http";
 import { connectDb } from './configs/DatabaseConfig.js';
-import userRoute from "./routes/userRoutes.js";
-import noteRoute from "./routes/noteRoutes.js";
+import router from "./routes/router.js";
 import dotenv from 'dotenv'
 dotenv.config()
 
 connectDb();
 
 const server = createServer((req, res) => {
-  const { url, method } = req;
-
-  if (url.startsWith("/api/v1/users")) {
-    userRoute(req, res);
-  } else if (url.startsWith("/api/v1/notes")) {
-    noteRoute(req, res);
-  } else {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "Route not found" }));
-  }
+  router(req, res);
 });
 
 server.listen(8080, () => {
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,76 @@
+import { login, register, getUserDetails, getUserProfilePicture, update } from '../controllers/userController.js';
+import { saveNote, updateNote, findNoteById, deleteNoteById, findNoteByUser } from "../controllers/noteController.js";
 import { saveSection, getSections, deleteBySectionId } from "../controllers/sectionController.js";
 
 export default function router(req, res) {
   const { method, url } = req;
   const parsedUrl = new URL(url, `http://${req.headers.host}`);
   const pathSegments = parsedUrl.pathname.split("/").filter(Boolean);
+
+  if (method === 'GET' && pathSegments.length === 4 &&
+    pathSegments[0] === "api" && pathSegments[1] === "v1" &&
+    pathSegments[2] === "users" && pathSegments[3] === "details") {
+    return getUserDetails(req, res);
+  }
+
+  if (method === "GET" && pathSegments.length === 4 &&
+    pathSegments[0] === "api" && pathSegments[1] === "v1" &&
+    pathSegments[2] === "users" && pathSegments[3] === "photo") {
+    return getUserProfilePicture(req, res);
+  }
+
+  if (method === "POST" && pathSegments.length === 4 &&
+    pathSegments[0] === "api" && pathSegments[1] === "v1" &&
+    pathSegments[2] === "users" && pathSegments[3] === "register") {
+    return register(req, res);
+  }
+
+  if (method === 'POST' && pathSegments.length === 4 &&
+    pathSegments[0] === "api" && pathSegments[1] === "v1" &&
+    pathSegments[2] === "users" && pathSegments[3] === "login") {
+    return login(req, res);
+  }
+
+  if (method === "PUT" && pathSegments.length === 3 &&
+    pathSegments[0] === "api" && pathSegments[1] === "v1" &&
+    pathSegments[2] === "users") {
+    return update(req, res);
+  }
+  if (method === "GET" && pathSegments.length === 3 &&
+    pathSegments[0] === "api" && pathSegments[1] === "v1" &&
+    pathSegments[2] === "notes") {
+    return findNoteByUser(req, res);
+  }
+
+  if (method === "GET" && pathSegments.length === 4 &&
+    pathSegments[0] === "api" && pathSegments[1] === "v1" &&
+    pathSegments[2] === "notes") {
+    const id = pathSegments[3];
+    req.id = id;
+    return findNoteById(req, res);
+  }
+
+  if (method === "POST" && pathSegments.length === 3 &&
+    pathSegments[0] === "api" && pathSegments[1] === "v1" &&
+    pathSegments[2] === "notes") {
+    return saveNote(req, res);
+  }
+
+  if (method === "PUT" && pathSegments.length === 4 &&
+    pathSegments[0] === "api" && pathSegments[1] === "v1" &&
+    pathSegments[2] === "notes") {
+    const id = pathSegments[3];
+    req.id = id;
+    return updateNote(req, res);
+  }
+
+  if (method === "DELETE" && pathSegments.length === 4 &&
+    pathSegments[0] === "api" && pathSegments[1] === "v1" &&
+    pathSegments[2] === "notes") {
+    const id = pathSegments[3];
+    req.id = id;
+    return deleteNoteById(req, res);
+  }
 
   if (method === "GET" && pathSegments.length === 5 &&
     pathSegments[0] === "api" && pathSegments[1] === "v1" &&
