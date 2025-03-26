91b4b52e0d76aef6bbdf5edae75235ed61d4dd39
ThienPhuc <thienphuc484@gmail.com>
2025-03-26 10:08:49 +0700

    refactor(CORS): remove CORS in index and add to nginx.conf

--- src/index.js
+++ src/index.js
@@ -10,29 +10,6 @@ connectDb();
 getRedisClient();
 
 const server = createServer((req, res) => {
-  const allowedOrigin = "http://localhost:4201";
-  const allowedMethods = ['GET','POST', 'PUT', 'DELETE', 'OPTIONS'];
-
-  res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
-  res.setHeader('Access-Control-Allow-Methods', allowedMethods.join(','));
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  if (req.method === 'OPTIONS') {
-    res.writeHead(204);
-    res.end();
-    return;
-  }
-
-  if (req.headers.origin !== allowedOrigin) {
-    res.writeHead(403, { "Content-Type" : "application/json"});
-    return res.end(JSON.stringify({ message : 'Forbidden: Invalid origin'}));
-  }
-
-  if (!allowedMethods.includes(req.method)) {
-    res.writeHead(405, { "Content-Type" : "application/json"});
-    return res.end(JSON.stringify({ message : 'Method Not Allowed'}));
-  }
-
   router(req, res);
 });
 
