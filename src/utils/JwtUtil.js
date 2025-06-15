d66a9524576f75cc0f30a588bc15b9863bc46ddd
thienphuc <ntphuc5484@gmail.com>
2025-06-15 11:32:44 +0700

    refactor: jwtUtils

--- src/utils/JwtUtil.js
+++ src/utils/JwtUtil.js
@@ -1,27 +1,22 @@
 import jsonwebtoken from "jsonwebtoken";
 
-export function extractTokenFromHeader(req) {
+export function verifyJwt(req, res, next) {
   const jwtSecretKey = process.env.JWT_SECRET_KEY;
 
   const authHeader = req.headers["authorization"];
-  if (!authHeader) {
-    const error = new Error("Authorization header missing");
-    error.statusCode = 400;
-    throw error;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    res.writeHead(401, { "Content-Type": "Application/json" });
+    return res.end(JSON.stringify({ error: "missing or invalid authorization header" }));
   }
 
   const token = authHeader.split(' ')[1];
-  if (!token) {
-    const error = new Error("Token missing");
-    error.statusCode = 400;
-    throw error;
-  }
-  console.log("Received Token:", token);
-  try {
-    return jsonwebtoken.verify(token, jwtSecretKey);
-  } catch (err) {
-    err.statusCode = 401;
-    err.details = "Invalid or expired token";
-    throw err;
-  }
+  jsonwebtoken.verify(token, jwtSecretKey, (err, decoded) => {
+    if (err) {
+      res.writeHead(403, { "Content-Type": "Application/json" });
+      return res.end(JSON.stringify({ error: "invalid or expired token" }));
+    }
+
+    req.user = decoded;
+    next();
+  });
 }
